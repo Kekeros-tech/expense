5b8650fde24a071ea899adaf4ee424cc2fd54563
Gleb Bukanov <69265505+Kekeros-tech@users.noreply.github.com>
2026-04-14 16:26:32 +0300

    [FT-4] Implement log support

--- local_run.bat
+++ local_run.bat
@@ -1,13 +1,18 @@
 @echo off
 setlocal
-
+cd /d "%~dp0"
 :: ================= Settings =================
 if not defined APP_JAR (
     set "APP_JAR=target\expense-0.0.1-SNAPSHOT.jar"
 )
+if not defined LOG_PATH (
+    set "LOG_PATH=.\.logs"
+)
+if not exist "%LOG_PATH%" (
+    mkdir "%LOG_PATH%"
+)
 set "JAVA_OPTS=-Xms512m -Xmx1024m -XX:+UseG1GC -Dfile.encoding=UTF-8"
 :: =============================================
-cd /d "%~dp0"
 
 java -version >nul 2>&1
 if %errorlevel% neq 0 (
@@ -25,7 +30,8 @@ if not exist "%APP_JAR%" (
 echo.
 echo ==========================================
 echo [INFO] Start Spring application...
-echo [INFO] PATH: %APP_JAR%
+echo [INFO] APP_JAR: %APP_JAR%
+echo [INFO] LOG_PATH: %LOG_PATH%
 echo ==========================================
 echo.
 
